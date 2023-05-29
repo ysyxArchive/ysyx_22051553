1ed5b2a01a82fe7021543f78d8ad2e678757b44a
tracer-ysyx <tracer@ysyx.org>
2023-05-29 23:19:53 +0800

    > sim RTL ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:19:53 up  2:41,  1 user,  load average: 1.19, 1.09, 0.79

--- npc/lab2_en_de/src/Seg.scala
+++ npc/lab2_en_de/src/Seg.scala
@@ -8,19 +8,19 @@ class Seg extends Module{  //Module会有clk和rst  //共阳
     })
 
     val mapArray = Seq(
-        (io.data_in === 0.U) -> Reverse("b0000001".U),   
-        (io.data_in === 1.U) -> Reverse("b1001111".U),
-        (io.data_in === 2.U) -> Reverse("b0010010".U),
-        (io.data_in === 3.U) -> Reverse("b0000110".U),
-        (io.data_in === 4.U) -> Reverse("b1001100".U),
-        (io.data_in === 5.U) -> Reverse("b0100100".U),
-        (io.data_in === 6.U) -> Reverse("b0100000".U),
-        (io.data_in === 7.U) -> Reverse("b0001111".U),
-        (io.data_in === 8.U) -> Reverse("b0000000".U),
-        (io.data_in === 9.U) -> Reverse("b0000100".U)
+        (io.data_in === 0.U) -> ("b0000001".U),   
+        (io.data_in === 1.U) -> ("b1001111".U),
+        (io.data_in === 2.U) -> ("b0010010".U),
+        (io.data_in === 3.U) -> ("b0000110".U),
+        (io.data_in === 4.U) -> ("b1001100".U),
+        (io.data_in === 5.U) -> ("b0100100".U),
+        (io.data_in === 6.U) -> ("b0100000".U),
+        (io.data_in === 7.U) -> ("b0001111".U),
+        (io.data_in === 8.U) -> ("b0000000".U),
+        (io.data_in === 9.U) -> ("b0000100".U)
     )
 
-    io.encode_out := MuxCase(Reverse("b0000001".U),
+    io.encode_out := MuxCase(("b0000001".U),
         mapArray
     )
 
