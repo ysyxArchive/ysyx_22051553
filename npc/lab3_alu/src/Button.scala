cf32add4c186e271a5f995a72a1824ae90070036
tracer-ysyx <tracer@ysyx.org>
2023-06-04 21:08:10 +0800

    > generate verilog ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:10 up 10:32,  1 user,  load average: 0.29, 0.19, 0.18

--- npc/lab3_alu/src/Button.scala
+++ npc/lab3_alu/src/Button.scala
@@ -56,19 +56,7 @@ class Button extends Module{
                 && (buffer(9,1).xorR) //odd    //R应该是指类似Reduce方法
             ){
                 io.button_out := 
-                    MuxCase(
-                        0.U(3.W),
-                        Seq(
-                            (buffer(8,1) === Button.a) -> ALU_ADD,
-                            (buffer(8,1) === Button.b) -> ALU_SUB,
-                            (buffer(8,1) === Button.c) -> ALU_NOT,
-                            (buffer(8,1) === Button.d) -> ALU_AND,
-                            (buffer(8,1) === Button.e) -> ALU_OR,
-                            (buffer(8,1) === Button.f) -> ALU_XOR,
-                            (buffer(8,1) === Button.g) -> ALU_COM,
-                            (buffer(8,1) === Button.h) -> ALU_EUQ
-                        )
-                    )
+                    0.U(3.W)
             }
         
             count := 0.U
