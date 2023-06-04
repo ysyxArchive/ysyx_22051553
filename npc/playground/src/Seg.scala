700caf9029bcdaffab481091df28a33b7adefcf8
tracer-ysyx <tracer@ysyx.org>
2023-06-04 23:25:34 +0800

    > generate verilog ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:34 up 12:50,  1 user,  load average: 0.28, 0.30, 0.27

--- npc/playground/src/Seg.scala
+++ npc/playground/src/Seg.scala
@@ -6,31 +6,26 @@ class Seg extends Module{  //Module会有clk和rst  //共阳
         val dataIn      = Input(Vec(4,UInt(4.W)))
         val encodeOut   = Output(Vec(4,UInt(7.W)))
     })
+    
 
-    io.encodeOut(0) := 0.U(7.W)
-    io.encodeOut(1) := 0.U(7.W)
-    io.encodeOut(2) := 0.U(7.W)
-    io.encodeOut(3) := 0.U(7.W)
+    // def genMapArray(data: UInt):UInt = {
+    //     val MapArray = Seq(
+    //         (data === 0.U) -> ("b0000001".U(7.W)),   //"b abcdefg"
+    //         (data === 1.U) -> ("b1001111".U(7.W)),
+    //         (data === 2.U) -> ("b0010010".U(7.W)),
+    //         (data === 3.U) -> ("b0000110".U(7.W)),
+    //         (data === 4.U) -> ("b1001100".U(7.W)),
+    //         (data === 5.U) -> ("b0100100".U(7.W)),
+    //         (data === 6.U) -> ("b0100000".U(7.W)),
+    //         (data === 7.U) -> ("b0001111".U(7.W)),
+    //         (data === 8.U) -> ("b0000000".U(7.W)),
+    //         (data === 9.U) -> ("b0000100".U(7.W))
+    //     )
 
-
-    def genMapArray(data: UInt):UInt = {
-        val MapArray = Seq(
-            (data === 0.U) -> ("b0000001".U(7.W)),   //"b abcdefg"
-            (data === 1.U) -> ("b1001111".U(7.W)),
-            (data === 2.U) -> ("b0010010".U(7.W)),
-            (data === 3.U) -> ("b0000110".U(7.W)),
-            (data === 4.U) -> ("b1001100".U(7.W)),
-            (data === 5.U) -> ("b0100100".U(7.W)),
-            (data === 6.U) -> ("b0100000".U(7.W)),
-            (data === 7.U) -> ("b0001111".U(7.W)),
-            (data === 8.U) -> ("b0000000".U(7.W)),
-            (data === 9.U) -> ("b0000100".U(7.W))
-        )
-
-        val out = MapArray.collectFirst{case (cond, value) if (cond == true.B) => value}.getOrElse("b0000001".U(7.W))
-        //模式匹配 -- cond只能为Boolean类型
-        out
-    }
+    //     val out = MapArray.collectFirst{case (cond, value) if (cond == true.B) => value}.getOrElse("b0000001".U(7.W))
+    //     //模式匹配 -- cond只能为Boolean类型
+    //     out
+    // }
 
     // val mapArray = Seq(
     //     (io.data_in === 0.U) -> ("b0000001".U),   //"b abcdefg"
@@ -48,9 +43,26 @@ class Seg extends Module{  //Module会有clk和rst  //共阳
 
 
 
-    io.encodeOut(0) := genMapArray(io.dataIn(0))
-    io.encodeOut(1) := genMapArray(io.dataIn(1))
-    io.encodeOut(2) := genMapArray(io.dataIn(2))
-    io.encodeOut(3) := genMapArray(io.dataIn(3))
+    // io.encodeOut(0) := genMapArray(io.dataIn(0))
+    // io.encodeOut(1) := genMapArray(io.dataIn(1))
+    // io.encodeOut(2) := genMapArray(io.dataIn(2))
+    // io.encodeOut(3) := genMapArray(io.dataIn(3))
+
+
+
+     val lut = VecInit("b1111110".U,
+                    "b0110000".U,
+                    "b1101101".U,
+                    "b1111001".U,
+                    "b0110011".U,
+                    "b1011011".U,
+                    "b1011111".U,
+                    "b1110000".U,
+                    "b1111111".U,
+                    "b1111011".U)
+
+  for (i <- 0 until 4) {
+    io.encodeOut(i) := lut(io.dataIn(i))
+  }
 
 }
