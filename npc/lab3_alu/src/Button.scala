a88349924597f1e2552358b663036a0012ce9542
tracer-ysyx <tracer@ysyx.org>
2023-06-04 21:58:08 +0800

    > generate verilog ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:58:08 up 11:22,  1 user,  load average: 1.03, 0.48, 0.30

--- npc/lab3_alu/src/Button.scala
+++ npc/lab3_alu/src/Button.scala
@@ -58,7 +58,7 @@ class Button extends Module{
                     MuxCase(
                         0.U(3.W),
                         Seq(
-                             (buffer(8,1) === "0x11".U) -> ALU_ADD        //注意从左到右的优先级
+                             (buffer(8,1) === ("0x11".U)) -> ALU_ADD        //注意从左到右的优先级
                            
                         )
                     )
