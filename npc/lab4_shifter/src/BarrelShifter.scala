1bd7ff433221443c30f2f3e1c7b94253b05608ef
tracer-ysyx <tracer@ysyx.org>
2023-06-07 21:27:42 +0800

    > generate verilog ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:27:42 up  3:29,  1 user,  load average: 0.70, 0.36, 0.24

---
+++ npc/lab4_shifter/src/BarrelShifter.scala
@@ -0,0 +1,62 @@
+import chisel3._
+import chisel3.util._
+
+class BSIO extends Bundle{
+    val Din         = Input(UInt(8.W))
+    val shamt       = Input(UInt(3.W))
+    val LR          = Input(UInt(1.W))
+    val AL          = Input(UInt(1.W))
+    val Dout        = Output(UInt(8.W))
+}
+
+
+class BarrelShifter extends Module{
+  
+    val io = IO(new BSIO)
+
+
+    //------first stage
+    val A = Wire(UInt(8.W))     //用和不用Wire有什么区别
+    
+    A := MuxLookup(
+        Cat(io.LR,io.shamt(0)),
+        0.U,
+        Seq(
+            0.U -> io.Din,
+            1.U -> Cat(io.AL,io.Din(7,1)),
+            2.U -> io.Din,
+            3.U -> Cat(io.Din(6,0),0.U(1.W))
+        )
+    )
+
+    //------second stage
+    val B = Wire(UInt(8.W))     //用和不用Wire有什么区别
+
+    B := MuxLookup(
+        Cat(io.LR,io.shamt(1)),
+        0.U,
+        Seq(
+            0.U -> A,
+            1.U -> Cat(io.AL,io.AL,A(7,2)),
+            2.U -> A,
+            3.U -> Cat(A(5,0),0.U(2.W))
+        )
+    )
+
+    //------third stage
+    val C = Wire(UInt(8.W))     //用和不用Wire有什么区别
+
+    C := MuxLookup(
+        Cat(io.LR,io.shamt(2)),
+        0.U,
+        Seq(
+            0.U -> B,
+            1.U -> Cat(io.AL,io.AL,io.AL,io.AL,B(7,4)),
+            2.U -> B,
+            3.U -> Cat(B(3,0),0.U(4.W))
+        )
+    )
+    
+
+    io.Dout := C
+}
