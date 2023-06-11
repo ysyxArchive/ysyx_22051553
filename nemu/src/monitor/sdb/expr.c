2659d4eddfa721590ef1ebb1d568743a26661dac
tracer-ysyx <tracer@ysyx.org>
2023-06-12 00:35:06 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:35:06 up  8:44,  1 user,  load average: 1.76, 1.09, 0.62

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -44,7 +44,7 @@ static struct rule {
   {"==", TK_EQ},          // equal
   {"\\(", '('},           // left
   {"\\)", ')'},           // right
-  {"[^(0x)][0-9]+", TK_DEC}, // DEC
+  {"[^(0x) ][0-9]+", TK_DEC}, // DEC
   {"(0x)[0-9]+", TK_HEX}  // HEX
 };
 
