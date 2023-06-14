bc91ba3e8b87ba3c454ec32bb92afb8b10f66a8c
tracer-ysyx <tracer@ysyx.org>
2023-06-14 16:59:53 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:53 up  7:14,  1 user,  load average: 0.27, 0.20, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -168,7 +168,7 @@ static bool make_token(char *e) {
       }
 
 
-      if (i == NR_REGEX) {
+      else if (i == NR_REGEX) {
         printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
         return false;
       }
