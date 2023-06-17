d1458be18cf68b34802593b1e8ccb68ef5bc0df6
tracer-ysyx <tracer@ysyx.org>
2023-06-18 02:09:14 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:09:14 up 12:26,  1 user,  load average: 0.27, 0.20, 0.14

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -74,6 +74,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J: src1R();          immJ(); break;
     case TYPE_R: src1R(); src2R();         break;
+    case TYPE_N:                           break;
   }
 }
 
