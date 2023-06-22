17b94a3c5b70479d50cb50860f6a8987d08f9a80
tracer-ysyx <tracer@ysyx.org>
2023-06-22 10:31:39 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-45-generic #46~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 7 15:06:04 UTC 20 x86_64 x86_64 x86_64 GNU/Linux  10:31:39 up 2 min,  1 user,  load average: 0.53, 0.40, 0.17

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,129 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+	assert(s != NULL);
+	size_t length = 0;
+	while(*(s+length) != '\0') length++;
+	return length;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+	assert(dst!=NULL && src!= NULL);
+	size_t offset = 0;
+	while(*(src+offset) != '\0'){
+		*(dst+offset) = *(src+offset);
+		offset ++;
+	}
+	*(dst+offset) = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  size_t offset = 0;
+  int 	end_flag = 0;
+  while(offset < n){
+	if(*(src+offset) == '\0') end_flag = 1;
+	*(dst+offset) = (end_flag) ? '\0' : *(src+offset);
+	offset ++;
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  int begin = 0;
+  int offset = 0;
+  while(*(dst+begin) != '\0') begin ++;
+  while(*(src+offset) != '\0'){
+    *(dst+begin+offset) = *(src+offset);
+    offset ++;
+  }
+  *(dst+begin+offset) = '\0';
+
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int offset = 0;
+  while(*(s1+offset) != '\0' && *(s2+offset) != '\0' &&
+    (*(s1+offset) == *(s2+offset))) offset ++;
+  
+  if(*(s1+offset) == '\0' && *(s2+offset) == '\0'){
+    return 0;
+  }else if(*(s1+offset) == '\0' && *(s2+offset) != '\0'){
+    return -1;
+  }else if(*(s1+offset) != '\0' && *(s2+offset) == '\0'){
+    return 1;
+  }
+  else return (*(s1 + offset) < *(s2 + offset)) ? -1 : 1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+	int offset = 0;
+	while(*(s1+offset) != '\0' && *(s2+offset) != '\0' &&
+	(*(s1+offset) == *(s2+offset)) && offset < n) offset ++;
+
+	if(offset == n)
+	return 0;
+	else if(*(s1+offset) == '\0' && *(s2+offset) == '\0'){
+	return 0;
+	}else if(*(s1+offset) == '\0' && *(s2+offset) != '\0'){
+	return -1;
+	}else if(*(s1+offset) != '\0' && *(s2+offset) == '\0'){
+	return 1;
+	}
+	else return (*(s1 + offset) < *(s2 + offset)) ? -1 : 1;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  int offset = 0;
+  while(offset < n){
+    *((char*)s+offset) = c;
+    offset ++;
+  }
+  return s;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+void *memmove(void *dst, const void *src, size_t n) {  //确保重叠不产生数据损坏
+  int offset = 0;
+  char *d = (char *)dst;
+  const char *s = (const char *)src;
+
+  if (d < s) {
+    while (offset < n) {
+      d[offset] = s[offset];
+      offset++;
+    }
+  } else {
+    offset = n - 1;
+    while (offset >= 0) {
+      d[offset] = s[offset];
+      offset--;
+    }
+  }
+
+  return dst;
 }
+
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  int offset = 0;
+  while(offset < n){
+	*(char*)(out+offset) = *(char *)(in+offset);
+	offset ++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  int offset = 0;
+  while( *((char*)s1+offset) == *((char*)s2+offset)  && offset < n){
+    offset ++;
+  }
+  if(offset == n)return 0;
+  else if(*((char*)s1+offset) > *((char*)s2+offset))return 1;
+  else if(*((char*)s1+offset) < *((char*)s2+offset))return -1;
+
+  return 0;
 }
 
 #endif
