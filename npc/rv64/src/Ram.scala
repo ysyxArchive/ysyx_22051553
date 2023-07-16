7a5341221e4d9c107876585274cede1136d6dab4
tracer-ysyx <tracer@ysyx.org>
2023-07-16 16:24:45 +0800

    > generate verilog ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:45 up  5:38,  3 users,  load average: 0.56, 0.48, 0.36

--- npc/rv64/src/Ram.scala
+++ npc/rv64/src/Ram.scala
@@ -14,7 +14,7 @@ class Ram extends Module{
     val io = IO(new RamIO) //在没有extend Module时，IO报错
    
     val SyncMem = SyncReadMem(256, UInt(PC_LEN.W))
-    loadMemoryFromFile(SyncMem, "/home/shikye/ysyx-workbench/npc/rv64/sim/Mem.txt")
+    loadMemoryFromFileInline(SyncMem, "/home/shikye/ysyx-workbench/npc/rv64/sim/Mem.txt")
 
     val inst_valid = RegNext(io.pc.valid)
     io.dataOut.valid := inst_valid
