42711ac99d5d326359f50023e1b72f579b16e7ba
tracer-ysyx <tracer@ysyx.org>
2023-07-26 02:25:45 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:25:45 up  3:10,  1 user,  load average: 0.49, 0.33, 0.27

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -1,12 +1,15 @@
 #include <common.h>
 #include <elf.h>
 
-char shstrtab[512];
-char strtab[512];
-char symtab[512];
+
 FILE *elf_fp = NULL;
 
 void init_elf(const char *elf_file){
+
+    char shstrtab[512];
+    char strtab[512];
+    Elf64_Sym symtab[128];
+
     int avoid_warning = 0;
     
     Elf64_Ehdr ehdr;   //elf头结构
@@ -17,17 +20,35 @@ void init_elf(const char *elf_file){
 
     elf_fp = fopen(elf_file, "r");
     Assert(elf_fp, "Can not open '%s'", elf_file);
-    avoid_warning = fread(&ehdr, 1, sizeof(Elf64_Ehdr), elf_fp);
+    avoid_warning = fread(&ehdr, 1, sizeof(Elf64_Ehdr), elf_fp);    //elf头
 
-    Elf64_Shdr shdr[ehdr.e_shnum];
+    Elf64_Shdr shdr[ehdr.e_shnum];              //段表
     fseek(elf_fp, ehdr.e_shoff, SEEK_SET);
     for(int i = 0; i < ehdr.e_shnum; i++){
         avoid_warning = fread(&shdr[i], 1, sizeof(Elf64_Shdr), elf_fp);
     }
 
-    Elf64_Shdr shdr_shstrtab = shdr[ehdr.e_shstrndx];
+    Elf64_Shdr shdr_shstrtab = shdr[ehdr.e_shstrndx];       //段表字符串表
     fseek(elf_fp, shdr_shstrtab.sh_offset, SEEK_SET);
     avoid_warning = fread(shstrtab, 1, shdr_shstrtab.sh_size, elf_fp);
-    printf("%s, %d", shstrtab, avoid_warning);
+    
+    for(int i = 0; i < ehdr.e_shnum; i++){                  //字符串表
+        if( strcmp(".strtab", &shstrtab[shdr[i].sh_name]) == 0){
+            fseek(elf_fp, shdr[i].sh_offset, SEEK_SET);
+            avoid_warning = fread(strtab, 1, shdr[i].sh_size, elf_fp);
+            break;
+        }
+    }
+
+    for(int i = 0; i < ehdr.e_shnum; i++){                  //符号表
+        if( strcmp(".symtab", &shstrtab[shdr[i].sh_name]) == 0){
+            fseek(elf_fp, shdr[i].sh_offset, SEEK_SET);
+            avoid_warning = fread(symtab, shdr[i].sh_size/shdr[i].sh_entsize, shdr[i].sh_entsize, elf_fp);
+            assert(avoid_warning == shdr[i].sh_size/shdr[i].sh_entsize);
+            break;
+        }
+    }
+
+
     return ;
 }
