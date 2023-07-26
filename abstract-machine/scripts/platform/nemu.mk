bba230e88fdd6cecc6a6c62827394ff221b8668f
tracer-ysyx <tracer@ysyx.org>
2023-07-26 22:53:00 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:53:00 up 12:11,  1 user,  load average: 0.40, 0.47, 0.40

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -7,11 +7,11 @@ AM_SRCS := platform/nemu/trm.c \
            platform/nemu/ioe/disk.c \
            platform/nemu/mpe.c
 
-CFLAGS    += -fdata-sections -ffunction-sections -DBATCH_MODE
+CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt --elf=$(abspath $(IMAGE).elf)  
+NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt --elf=$(abspath $(IMAGE).elf)  -DBATCH_MODE
 # 新增elf选项 批处理模式
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
