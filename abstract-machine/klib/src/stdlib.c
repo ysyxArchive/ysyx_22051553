d780ee3ad4b82072ec2efb803bbf70dbc87e3f10
tracer-ysyx <tracer@ysyx.org>
2023-07-29 20:34:10 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:34:10 up 56 min,  1 user,  load average: 0.36, 0.72, 0.67

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -36,7 +36,7 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
   if(hbrk == NULL)
-    hbrk = heap.start;
+    hbrk = (void *)ROUNDUP(heap.start, 8);
 
   size = (size_t)ROUNDUP(size, 8);
   char *old = hbrk;
