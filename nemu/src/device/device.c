6f8870868109990a4b54250e2adfd146187e470b
tracer-ysyx <tracer@ysyx.org>
2023-07-29 20:17:26 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:17:26 up 40 min,  1 user,  load average: 0.17, 0.23, 0.25

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -48,7 +48,7 @@ void device_update() {                //每执行一条指令，update一次
 #ifndef CONFIG_TARGET_AM
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
-    SDL_Delay(1);
+    printf("in sdl\n");
     switch (event.type) {
       case SDL_QUIT:
         nemu_state.state = NEMU_QUIT;
