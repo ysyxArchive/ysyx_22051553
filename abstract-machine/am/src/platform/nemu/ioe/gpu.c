2f3c54a81a49da36aa44a6361847c0061c93b606
tracer-ysyx <tracer@ysyx.org>
2023-07-30 02:02:15 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:02:15 up  6:25,  1 user,  load average: 0.73, 0.42, 0.37

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,7 +7,7 @@ void __am_gpu_init() {
   uint32_t vga_ctrl_bundle = inl(VGACTL_ADDR);
   int i;
   int w = vga_ctrl_bundle>>16;  // TODO: get the correct width
-  int h = vga_ctrl_bundle & 0xffff;  // TODO: get the correct height
+  int h = vga_ctrl_bundle & 0x0000ffff;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
