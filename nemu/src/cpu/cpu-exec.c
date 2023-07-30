bfb5d174e6e0b1ab3c7bf747ba837dbb6e24188e
tracer-ysyx <tracer@ysyx.org>
2023-07-30 11:58:55 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:58:55 up  2:28,  1 user,  load average: 0.66, 0.44, 0.32

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -78,13 +78,14 @@ static void exec_once(Decode *s, vaddr_t pc) {
 #else
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
-  printf("p2\n");
+  
   p = iringbuf[irb_pos];
   strcpy(p, "0x");
   p += 2;
   snprintf(p, 11, "%.10s", s->logbuf + 10);  //.10s表示最多打印10个字符，否则会Werror
   p += 10;
   strcpy(p, s->logbuf + 32);
+  printf("p2\n");
   irb_pos = (irb_pos == 15) ? 0 : irb_pos+1;
 #endif
   
