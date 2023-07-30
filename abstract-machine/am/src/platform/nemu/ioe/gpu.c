1d6ea591410366553cd3eb2885c55b58e1f9f160
tracer-ysyx <tracer@ysyx.org>
2023-07-30 11:07:27 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:07:27 up  1:37,  1 user,  load average: 0.44, 0.52, 0.39

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,11 +30,11 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   gpuptr_t *pixels = ctl->pixels;
 
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  int screen_w = inl(VGACTL_ADDR) >> 16;
+  // int screen_w = inl(VGACTL_ADDR) >> 16;
 
   for(int n = 0; n < ctl->h; n ++)
     for(int m = 0; m < ctl->w; m ++){   //行优先
-      outl((uintptr_t)(fb + ctl->x + (ctl->y)*(screen_w) + m + n*screen_w),
+      outl((uintptr_t)(fb + ctl->x + (ctl->y)*(400) + m + n*400),
       pixels[m+ n*(ctl->w)]);
     }
 
