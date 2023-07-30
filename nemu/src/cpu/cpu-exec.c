e27c52778379f72f2b7985adbe7b9f36ac7b218a
tracer-ysyx <tracer@ysyx.org>
2023-07-30 11:59:43 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:59:43 up  2:29,  1 user,  load average: 0.56, 0.46, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -52,9 +52,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
+  printf("p1\n");
   isa_exec_once(s);
   cpu.pc = s->dnpc;             //注意静态指令和动态指令    静态是pc+4,动态可能是跳转地址          注意pc指的就是当前的pc,和流水线硬件不一样        nemu是单周期模拟器   设置snpc、dnpc的原因是，pc在该指令完全模拟结束之前，还需要使用
-  printf("p1\n");
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;               //p指针不断后移    snprint返回值是写入的字符长度
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);      //"0x%016x" 将pc写入log
