0a4f940944b71b1447089569211c7af4b85cb6b4
tracer-ysyx <tracer@ysyx.org>
2023-07-30 11:51:26 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:51:26 up  2:21,  1 user,  load average: 0.41, 0.27, 0.22

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -92,6 +92,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 static void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
+    printf("p1\n");
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);                   //写log
@@ -128,7 +129,7 @@ void cpu_exec(uint64_t n) {
   uint64_t timer_start = get_time();
   
   execute(n);
-printf("p1\n");
+
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
 
