2e075e93bd113bc1a3ac85d5eae4c18bf1743267
tracer-ysyx <tracer@ysyx.org>
2023-07-31 09:45:54 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:45:54 up 22 min,  1 user,  load average: 0.54, 0.38, 0.25

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -33,7 +33,7 @@ uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 //iringbuf
-char iringbuf [16][128];
+char iringbuf [16][32];
 uint8_t irb_pos = 0;
 
 
