5bf11e5f536431462d891b923c14141af6b242b1
tracer-ysyx <tracer@ysyx.org>
2023-08-01 17:15:58 +0800

    > generate verilog ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:15:58 up  7:38,  1 user,  load average: 0.42, 0.45, 0.37

--- npc/rv64/src/rv64/Elaborate.scala
+++ npc/rv64/src/rv64/Elaborate.scala
@@ -4,7 +4,7 @@ import circt.stage._
 
 object Elaborate extends App {
   def top = new Soc()
-  val useMFC = true  // use MLIR-based firrtl compiler  -- 无法实例化loadMemory
+  val useMFC = false  // use MLIR-based firrtl compiler  -- 无法实例化loadMemory
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   if (useMFC) {
     (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
