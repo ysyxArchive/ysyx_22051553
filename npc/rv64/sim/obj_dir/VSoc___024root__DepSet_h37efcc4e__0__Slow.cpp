5543bcca431511270f1e413032bc85aafab810d8
tracer-ysyx <tracer@ysyx.org>
2023-08-03 16:14:33 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:14:33 up  6:22,  1 user,  load average: 0.38, 0.23, 0.19

--- npc/rv64/sim/obj_dir/VSoc___024root__DepSet_h37efcc4e__0__Slow.cpp
+++ npc/rv64/sim/obj_dir/VSoc___024root__DepSet_h37efcc4e__0__Slow.cpp
@@ -63,7 +63,7 @@ VL_ATTR_COLD void VSoc___024root___eval_settle(VSoc___024root* vlSelf) {
 #ifdef VL_DEBUG
                 VSoc___024root___dump_triggers__stl(vlSelf);
 #endif
-                VL_FATAL_MT("vsrc/Soc.v", 672, "", "Settle region did not converge.");
+                VL_FATAL_MT("vsrc/Soc.sv", 672, "", "Settle region did not converge.");
             }
             vlSelf->__VstlIterCount = ((IData)(1U) 
                                        + vlSelf->__VstlIterCount);
