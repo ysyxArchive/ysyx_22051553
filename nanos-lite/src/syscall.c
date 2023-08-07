23918a9bcf15eebe369153bc2e7bac168ae6b03e
tracer-ysyx <tracer@ysyx.org>
2023-08-07 13:16:14 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:16:14 up  2:43,  1 user,  load average: 0.37, 0.30, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,14 +6,20 @@ static void sys_yield(Context *c){
   c->GPRx = 0;
 }
 
+static void sys_exit(Context *c){
+  halt(c->GPRx);
+}
+
 
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
+  printf("syscall ID = %d\n", a[0]);
   switch (a[0]) {
-    case SYS_yield: printf("syscall ID = %d\n", a[0]); sys_yield(c); break;
+    case SYS_yield: sys_yield(c); break;
+    case SYS_exit: sys_exit(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
