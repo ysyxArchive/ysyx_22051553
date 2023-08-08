934542b761e84a1c36f7329e490a7969861a6407
tracer-ysyx <tracer@ysyx.org>
2023-08-08 12:42:18 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:42:18 up  1:42,  1 user,  load average: 0.33, 0.24, 0.24

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -9,7 +9,7 @@
 #define GPR2 gpr[10]
 #define GPR3 gpr[11]
 #define GPR4 gpr[12]
-#define GPRx gpr[17] // 返回值也是a7
+#define GPRx gpr[10] // 返回值是a0
 
 struct Context {
   // TODO: fix the order of these members to match trap.S
