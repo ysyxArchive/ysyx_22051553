c26b4099fd84f14f4c0c19467bcce8e9222eef28
tracer-ysyx <tracer@ysyx.org>
2023-08-08 11:26:09 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:26:09 up 25 min,  1 user,  load average: 0.30, 0.34, 0.27

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,6 +62,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
+
   int ret = _syscall_(SYS_write, fd, (intptr_t)buf, count);
 
   if(ret == count)
