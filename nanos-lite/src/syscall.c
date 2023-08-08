5992c0fbc3e7cabec065fdb0709638a4788fc2f0
tracer-ysyx <tracer@ysyx.org>
2023-08-08 11:19:32 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:19:32 up 19 min,  1 user,  load average: 0.35, 0.29, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,7 +6,22 @@ static void sys_yield(Context *c){
   c->GPRx = 0;
 }
 
+static void sys_write(Context *c){
+  if(c->GPR2 == 1 || c->GPR2 == 2){
+    for(int i = 0; i < c-> GPR4; i --){
+      putch(*((char *)(c->GPR3) + i));
+    }
+  }
+
+  c->GPRx = c->GPR4;
+}
+
 static void sys_exit(Context *c){
+  #ifdef CONFIG_STRACE
+  printf("syscall: %s over\nreturn value is 0x%lx\n", 
+  syscall_name[c->GPR1], c->GPRx);
+  #endif
+
   halt(c->GPRx);
 }
 
@@ -16,14 +31,20 @@ void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
+  #ifdef CONFIG_STRACE
   printf("syscall: %s happen\nparas:a0:0x%lx a1:0x%lx a2:0x%lx a7:0x%lx\n", 
   syscall_name[a[0]], c->GPR2,  c->GPR3, c->GPR4, c->GPR1);
+  #endif
 
   switch (a[0]) {
     case SYS_yield: sys_yield(c); break;
-    case SYS_exit: sys_exit(c); break;
+    case SYS_exit: sys_exit(c); break;                     //不在该循环中打印返回值
+    case SYS_write: sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  #ifdef CONFIG_STRACE
   printf("syscall: %s over\nreturn value is 0x%lx\n", 
   syscall_name[a[0]], c->GPRx);
+  #endif
 }
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -62,8 +62,12 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _exit(SYS_write);
-  return 0;
+  int ret = _syscall_(SYS_write, fd, (intptr_t)buf, count);
+
+  if(ret == count)
+    return ret;
+  else
+    _exit(SYS_write);
 }
 
 void *_sbrk(intptr_t increment) {
