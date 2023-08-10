2766cb2ede0832aac9d91fd0a3a4a3a288253087
tracer-ysyx <tracer@ysyx.org>
2023-08-10 15:32:05 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:05 up  5:04,  1 user,  load average: 0.41, 0.29, 0.28

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -23,7 +23,7 @@ int mm_brk(uintptr_t brk) {
 
 void init_mm() {
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
-
+  printf("get in\n");
   Log("free physical pages starting from %p", pf);
 
 #ifdef HAS_VME
