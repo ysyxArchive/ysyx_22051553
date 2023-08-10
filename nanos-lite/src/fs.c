a43416c411cc7e26d63db8fcddc5b53d407f7b9c
tracer-ysyx <tracer@ysyx.org>
2023-08-10 22:28:25 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:28:25 up 12:01,  1 user,  load average: 0.14, 0.18, 0.21

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -22,7 +22,7 @@ typedef struct {
 } Finfo;
 
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR,  FD_EVENTS,  FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
