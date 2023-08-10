895d1fd5519a2d0914a27f131348d788b5947677
tracer-ysyx <tracer@ysyx.org>
2023-08-10 21:59:02 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:59:02 up 11:31,  1 user,  load average: 0.26, 0.38, 0.40

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,8 +25,9 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
 
-  int fd = fs_open(filename, 0, 0);
   printf("here\n");
+  int fd = fs_open(filename, 0, 0);
+  
   Elf_Ehdr elf_header;
   assert(sizeof(Elf_Ehdr) ==  fs_read(fd, &elf_header, sizeof(Elf_Ehdr)));
 
