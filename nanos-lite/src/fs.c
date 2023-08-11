688af2d2c298f89b61f9a160aff5fddc3a594230
tracer-ysyx <tracer@ysyx.org>
2023-08-11 17:10:11 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:11 up  7:31,  1 user,  load average: 0.25, 0.41, 0.34

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -64,7 +64,7 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 size_t fs_read(int fd, void *buf, size_t len){
-
+printf("is reading %d\n", fd);
   
   if(fd <= FD_FB){
     return file_table[fd].read(buf, 0, len);
