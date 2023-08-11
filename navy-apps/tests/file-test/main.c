13fe109a5f94641f4d08b77e2fd4ad2ecc10cd2a
tracer-ysyx <tracer@ysyx.org>
2023-08-11 17:13:34 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:13:34 up  7:34,  1 user,  load average: 0.12, 0.29, 0.31

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -22,7 +22,7 @@ int main() {
   //    assert(n == i + 1);
   // }
     fscanf(fp, "%d", &n);
- 
+    fscanf(fp, "%d", &n);
   // fseek(fp, 0, SEEK_SET);
   // for (i = 0; i < 500; i ++) {
   //   fprintf(fp, "%4d\n", i + 1 + 1000);
