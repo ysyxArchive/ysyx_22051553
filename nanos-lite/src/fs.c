1c121ca25a84046e6c84ea1ca845e47d6ca826db
tracer-ysyx <tracer@ysyx.org>
2023-08-11 17:45:10 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:45:10 up  8:06,  1 user,  load average: 0.15, 0.15, 0.17

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -81,7 +81,7 @@ size_t fs_read(int fd, void *buf, size_t len){
     ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, real_len);
     file_table[fd].open_offset += real_len;
 
-    printf("offset = %d\n", file_table[fd].open_offset);
+    // printf("offset = %d\n", file_table[fd].open_offset);
     
     return real_len;
   }
