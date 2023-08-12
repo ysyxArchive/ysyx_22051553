ee2261d33e0a5c4ce0d34a10b02e8d42a7638203
tracer-ysyx <tracer@ysyx.org>
2023-08-12 12:53:47 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jul 13 16:27:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:53:47 up 40 min,  1 user,  load average: 0.28, 0.28, 0.22

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,53 +2,35 @@
 #include <assert.h>
 
 int main() {
-
-  
-
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
-  // int n;
-  // fscanf(fp, "%d", &n);
-  // printf("n = %d\n", n);
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  assert(size == 5000);
 
-  // long off = fseek(fp, 0, SEEK_END);
-  // long size = ftell(fp);
-  // assert(size == 5000);
-
-  // fseek(fp, 500 * 5, SEEK_SET);
-
+  fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
-
-  // for (i = 500; i < 502; i ++) {
-  //   fscanf(fp, "%d", &n);
-  //    assert(n == i + 1);
-  // }
-    // fscanf(fp, "%d", &n);
+  for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    printf("%d\n", n);
+    assert(n == i + 1);
+  }
+
+  fseek(fp, 0, SEEK_SET);
+  for (i = 0; i < 500; i ++) {
+    fprintf(fp, "%4d\n", i + 1 + 1000);
+  }
+
+  for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    printf("%d\n", n);
+    assert(n == i + 1);
+  }
+
+  fseek(fp, 0, SEEK_SET);
+  for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
-    printf("%d\n", n);
-
-
-    
-  // fseek(fp, 0, SEEK_SET);
-  // for (i = 0; i < 500; i ++) {
-  //   fprintf(fp, "%4d\n", i + 1 + 1000);
-  // }
-
-  // for (i = 500; i < 1000; i ++) {
-  //   fscanf(fp, "%d", &n);
-  //   assert(n == i + 1);
-  // }
-
-  // fseek(fp, 0, SEEK_SET);
-  // for (i = 0; i < 500; i ++) {
-  //   fscanf(fp, "%d", &n);
-  //   assert(n == i + 1 + 1000);
-  // }
+    assert(n == i + 1 + 1000);
+  }
 
   fclose(fp);
 
