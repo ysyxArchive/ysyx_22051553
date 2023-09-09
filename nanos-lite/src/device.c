67bd34e1ad135280e69f266566ae5356453e3e9d
tracer-ysyx <tracer@ysyx.org>
2023-09-09 17:36:50 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:36:50 up  2:43,  1 user,  load average: 0.53, 0.45, 0.38

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -96,7 +96,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int sys_w = ev_gpuconf.width;
 
 
-  io_write(AM_GPU_FBDRAW, offset%sys_w, offset/sys_w, (void *)buf, screen_w, screen_h, false);
+  io_write(AM_GPU_FBDRAW, offset%sys_w, offset/sys_w, (void *)buf, 400, 300, false);
 
   return 0;
 }
