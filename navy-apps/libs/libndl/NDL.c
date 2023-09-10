e867c9a5473ba23f9c1520dd07e6eb3ff8f4c2a5
tracer-ysyx <tracer@ysyx.org>
2023-09-10 21:17:44 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:44 up  3:24,  1 user,  load average: 0.25, 0.34, 0.41

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -79,15 +79,15 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) { //NDL_DrawRect
   printf("fbdev is %d\n", fbdev);
   lseek(fbdev,  ((disp_w-w)/2 + (disp_h-h)/2*disp_w)*(sizeof(uint32_t)) , SEEK_SET);  //移动画布从左上角到中间
 
-  uint32_t pix = 0x000000ff;
-  for(int i = 0; i < 1; i ++){
-    write(fbdev, &pix, 4);  
-  }
-
-  // for(int i = 0; i < h; i++){
-  //   write(fbdev, pixels + i*w, w*sizeof(uint32_t));  
-  //   lseek(fbdev, (disp_w - w)*sizeof(uint32_t), SEEK_CUR);
+  // uint32_t pix = 0x000000ff;
+  // for(int i = 0; i < 1; i ++){
+  //   write(fbdev, &pix, 4);  
   // }
+
+  for(int i = 0; i < h; i++){
+    write(fbdev, pixels + i*w, w*sizeof(uint32_t));  
+    lseek(fbdev, (disp_w - w)*sizeof(uint32_t), SEEK_CUR);
+  }
   write(fbsync, 0, 0);
 }
 
