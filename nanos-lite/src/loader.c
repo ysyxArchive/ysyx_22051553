06c5f49a436a9a26d2b190ee2b99356838bef784
tracer-ysyx <tracer@ysyx.org>
2023-09-15 09:51:29 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:51:29 up 20 min,  1 user,  load average: 0.60, 1.18, 0.91

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,12 +27,16 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   int fd = fs_open(filename, 0, 0);
   
+  
   Elf_Ehdr elf_header;
   assert(sizeof(Elf_Ehdr) ==  fs_read(fd, &elf_header, sizeof(Elf_Ehdr)));
+  
 
   assert(*(uint32_t*)(elf_header.e_ident) == 0x464c457f);  //只读4个字节的方式，小端存储，结构体从[0]到[3]地址递增
   assert(elf_header.e_machine == EXPECT_TYPE);
   
+  
+
   Elf_Phdr elf_ph[elf_header.e_phnum];
   fs_lseek(fd, elf_header.e_phoff, SEEK_SET);
 
@@ -51,7 +55,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     }
   }
   
-
   fs_close(fd);
 
 
