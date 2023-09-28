84910c04a91df3e795760c51dbdb8ea2a88a0620
tracer-ysyx <tracer@ysyx.org>
2023-09-28 12:34:51 +0800

    > generate verilog ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:34:51 up  2:44,  1 user,  load average: 0.86, 0.42, 0.27

--- npc/rv64/src/rv64/Alu.scala
+++ npc/rv64/src/rv64/Alu.scala
@@ -110,14 +110,14 @@ class Alu extends Module {
         }
 
     bm_value := Mux(BM.io.out_valid, 
-        MuxCase(0.U,
+        MuxCase(0.S,
             Seq(
                 (io.alu_op === (ALU_MUL) || io.alu_op === (ALU_MULW)) -> BM.io.result_lo.asSInt,
                 (io.alu_op === (ALU_MULH) || io.alu_op === (ALU_MULHSU) ||
                     io.alu_op === (ALU_MULHU)) -> BM.io.result_hi.asSInt
             )
         ),
-    0.U)
+    0.S)
 
 
     res := Mux(BM.io.out_valid, bm_value, 
