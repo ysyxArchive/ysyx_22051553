febf383c443ef6b28c0425495303f184712854f2
tracer-ysyx <tracer@ysyx.org>
2023-09-30 14:54:30 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:54:30 up  4:55,  1 user,  load average: 1.14, 0.95, 0.91

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,6 +55,7 @@ static Finfo file_table[] __attribute__((used)) = {
 
 
 int fs_open(const char *pathname, int flags, int mode){
+  printf("path is %s\n", pathname);
   for(int i = 0; i < NR_FILES; i ++){
     if(strcmp(file_table[i].name, pathname) == 0){
       file_table[i].open_offset = 0;
