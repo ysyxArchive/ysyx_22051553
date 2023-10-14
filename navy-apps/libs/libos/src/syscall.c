f1c412608cd9a326ad8610f7080f77e4688a88f9
tracer-ysyx <tracer@ysyx.org>
2023-10-14 20:21:41 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:21:41 up 10:37,  1 user,  load average: 0.33, 0.35, 0.32

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -115,13 +115,16 @@ void *_sbrk(intptr_t increment) {  //总是分配8字节对齐的数据
   if(_syscall_(SYS_brk, increment, 0, 0) == 0){
     char * old = hbrk;
 
-    if(increment & 0x7 == 0){
+    // if(increment & 0x7 == 0){
+    //   hbrk += increment;
+    //   return old;
+    // }else{
+    //   hbrk = increment - increment & 0x7 + 8;
+    //   return old;
+    // }
       hbrk += increment;
       return old;
-    }else{
-      hbrk = increment - increment & 0x7 + 8;
-      return old;
-    }
+
   }
   else
     return (void *)-1;
