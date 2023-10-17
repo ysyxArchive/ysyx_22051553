d09d2c21e689e33f6663df6156b7eafd2ad063aa
tracer-ysyx <tracer@ysyx.org>
2023-10-18 00:18:42 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:18:42 up  1:32,  1 user,  load average: 0.57, 0.69, 0.55

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -49,7 +49,7 @@ static word_t pmem_read(paddr_t addr, int len) {
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
   if(addr >= 0x83153e00 && addr <= 0x83153e80){
-    printf("addr is 0x%x\n", addr);
+    Log("write mem at " FMT_PADDR " for %d bytes",addr, len);
     printf("data is 0x%x\n", *(uint8_t*)guest_to_host(0x83153e62));
   }
     
