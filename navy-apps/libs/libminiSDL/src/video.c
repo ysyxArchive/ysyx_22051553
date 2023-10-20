ec2341264d978a00cc4b9d91dfd83ed93e87712a
tracer-ysyx <tracer@ysyx.org>
2023-10-20 13:02:21 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:02:21 up  3:10,  1 user,  load average: 0.25, 0.32, 0.31

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -225,7 +225,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     for(int i = 0; i < h; i++) {
         for(int j = 0; j < w; j++) {
             // *pixel_ptr = s->format->palette->colors[*src_ptr].val;
-            *pixel_ptr = SDL_MapRGBA(0, s->format->palette->colors[*src_ptr].r, s->format->palette->colors[*src_ptr].g,
+            *pixel_ptr = SDL_MapRGBA(s->format, s->format->palette->colors[*src_ptr].r, s->format->palette->colors[*src_ptr].g,
             s->format->palette->colors[*src_ptr].b, s->format->palette->colors[*src_ptr].a);
             // printf("value is %08x\n", *pixel_ptr);
             pixel_ptr++;
