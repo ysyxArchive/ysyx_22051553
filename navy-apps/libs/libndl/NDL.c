15579b2f7bd936f1149c342090815eff0af64f33
tracer-ysyx <tracer@ysyx.org>
2023-10-20 17:39:49 +0800

    > compile NEMU ysyx_22051553 钟阳 Linux shikye-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:49 up  7:47,  1 user,  load average: 0.54, 0.49, 0.39

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,181 +4,158 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <assert.h>
+#include <fcntl.h>
 
+#define IS_NUM(ch) (ch >= '0' && ch <= '9')
 
-typedef struct my_pixel{
-  void * pixel_budle;
-  int w, h;
-}my_pixel;
-
-
-
-static int evtdev = -1;            // /dev/events
-static int fbdev = -1;             // /dev/fb 
-static int fbsync = -1;
+static int evtdev = -1;
+static int fbdev = -1;
+static int dispinfo_dev = -1;
 static int screen_w = 0, screen_h = 0;
-static int disp_w = 0, disp_h = 0;
 
+typedef struct size
+{
+  int w;
+  int h;
+} Size;
+Size disp_size;
 
-static uint64_t boot_time = 0;
+// static void get_disp_size()
+// {
+// #define DISPINFO_LEN 64
+//   char buf[DISPINFO_LEN];
+//   assert(read(dispinfo_dev, buf, DISPINFO_LEN) != 0);
+//   size_t i = 0;
+//   // printf("buf is %s\n", buf);
+//   for (; buf[i] != '\n'; ++i)
+//   {
+//     if (IS_NUM(buf[i]))
+//       disp_size.w = disp_size.w * 10 + (buf[i] - '0');
+//   }
+//   ++i;
+//   for (; buf[i] != '\n'; ++i)
+//   {
+//     if (IS_NUM(buf[i]))
+//       disp_size.h = disp_size.h * 10 + (buf[i] - '0');
+//   }
+//   assert(disp_size.w > 0 && disp_size.h <= 800);
+//   assert(disp_size.h > 0 && disp_size.h <= 640);
+// }
 
-static uint64_t get_time_internal() {
-  struct timeval now;
-  gettimeofday(&now, NULL);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
-  return us;
+// return ms
+uint32_t NDL_GetTicks()
+{
+  static struct timeval timeval;
+  static struct timezone timezone;
+  int ret = gettimeofday(&timeval, &timezone);
+  return timeval.tv_usec / 1000;
 }
 
-uint64_t get_time() {
-  if (boot_time == 0) boot_time = get_time_internal();
-  uint64_t now = get_time_internal();
-  return now - boot_time;
+int NDL_PollEvent(char *buf, int len)
+{
+  buf[0] = '\0';
+  assert(evtdev != -1);
+  int ret = read(evtdev, buf, len);
+  return ret;
 }
 
-uint32_t NDL_GetTicks() {  //1Tick->1ms
-  
-  uint64_t us = get_time();
-  return (uint32_t)(us/1000);
+void NDL_OpenCanvas(int *w, int *h)
+{
+  if (*w == 0 && *h == 0)
+  {
+    *w = disp_size.w;
+    *h = disp_size.h;
+  }
 
-}
-
-int NDL_PollEvent(char *buf, int len) {   //轮询？
-
-  int fd = open("/dev/events", 0, 0);   //对于native或者nanos,读取键盘的逻辑都是轮询
-
-  return read(fd, buf, len);
-}
-
-void NDL_OpenCanvas(int *w, int *h) {  // w、h为画布尺寸（以字节为单位）        
-  
-    // int fbctl = 4;               // 从ioe读取系统屏幕尺寸信息
-    int fbctl = open("/proc/dispinfo", 0);
-
-    if(*w == 0 || *h == 0){
-      screen_h = disp_h;        //以像素点为单位
-      screen_w = disp_w;
-
-      *h = disp_h;
-      *w = disp_w;
-    }  
-    else{
-      screen_w = *w; 
-      screen_h = *h;  //记录画布大小
-    }
-    
-
-  if (getenv("NWM_APP")) {   //NWM_APP才需要做mmap等操作
-    fbctl = open("/proc/dispinfo", 0);;
-    fbdev = open("/dev/fb", 0);;
-    screen_w = *w; screen_h = *h;
+  if (getenv("NWM_APP"))
+  {
+    int fbctl = 4;
+    fbdev = 5;
+    screen_w = *w;
+    screen_h = *h;
     char buf[64];
     int len = sprintf(buf, "%d %d", screen_w, screen_h);
     // let NWM resize the window and create the frame buffer
     write(fbctl, buf, len);
-    while (1) {
+    while (1)
+    {
       // 3 = evtdev
       int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
+      if (nread <= 0)
+        continue;
       buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
+      if (strcmp(buf, "mmap ok") == 0)
+        break;
     }
+
     close(fbctl);
   }
-
-
 }
 
-void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) { //NDL_DrawRect(x, 0, 0, w, h);即在中间显示
-
-  
-  // write(fbdev, pixels, w*h);
-  // write(fbsync, 0, 0);
-  // printf("x is %d, y is %d\n", x, y);
-  // printf("w is %d, h is %d\n", w, h);
-  // printf("disp_w is %d, disp_h is %d\n", disp_w, disp_h);
-  // lseek(fbdev, ((disp_w - w)/2 + (disp_h - h)/2 * disp_w)*sizeof(uint32_t) , SEEK_SET);  //移动画布从左上角到中间
-
-  lseek(fbdev, ( x+w)*sizeof(uint32_t) , SEEK_SET);  //移动画布从左上角到中间,修改后，标题正常
-
-  // for(int i = 0; i < h; i++){        //逐行写入，是因为对应glibc_write的普通文件操作，write(fbdev, pixels, w*h);并不能跳跃着写，写一个矩形
-    
-  //   write(fbdev, pixels + i*w, w*sizeof(uint32_t));    //以字节为单位
-  //   int off = lseek(fbdev, (disp_w - w)*sizeof(uint32_t), SEEK_CUR);
-  //   // printf("off is %d\n", off);
-    
-  // }
-
-  //为写fb特例化，其中，write(fd, buf, len)中，buf不是要写的数据，而是包含w、h信息的数据
-  my_pixel buf_budle = {
-    .pixel_budle = pixels,
-    .w = w,
-    .h = h
-  };
-
-  // printf("two pixel is %x, %x\n", pixels[2], pixels[3]);
-
-  write(fbdev, &buf_budle, w*h*sizeof(uint32_t));
-
-  write(fbsync, 0, 0);
-  
-}
-
-void NDL_OpenAudio(int freq, int channels, int samples) {
-}
-
-void NDL_CloseAudio() {
-}
-
-int NDL_PlayAudio(void *buf, int len) {
-  return 0;
-}
-
-int NDL_QueryAudio() {
-  return 0;
-}
-
-int NDL_Init(uint32_t flags) {
-
-
-  if (getenv("NWM_APP")) {
-    // evtdev = 3;
-    evtdev = open("/dev/events", 0);
+void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h)
+{
+  if (w == 0 && h == 0)
+  {
+    w = disp_size.w;
+    h = disp_size.h;
   }
-  
+  assert(w > 0 && w <= disp_size.w);
+  assert(h > 0 && h <= disp_size.h);
 
-  // fbdev = 5;
-  fbdev = open("/dev/fb", 0);
-  fbsync = open("/dev/fb_sync", 0);
-
-  //获取系统屏幕大小
-  char dispinfo[32];
-  
-  // read(4, dispinfo, 32);
-  int fbctl = open("/proc/dispinfo", 0);
-  read(fbctl, dispinfo, 32);
-
-  char* token = strtok(dispinfo, " :\n");         
-  while(token != NULL){
-    if(strcmp(token, "WIDTH")==0 ||  strcmp(token, "HEIGHT")==0){
-      if(strcmp(token, "WIDTH") == 0){
-        token = strtok(NULL, " :\n");
-        disp_w = atoi(token);
-      }
-      else if(strcmp(token, "HEIGHT")==0){
-        token = strtok(NULL, " :\n");
-        disp_h = atoi(token);
-      }
-    }
-    token = strtok(NULL, " :\n");
+  // write(1, "here\n", 10);
+  // printf("draw [%d, %d] to [%d, %d]\n", w, h, x, y);
+  for (size_t row = 0; row < h; ++row)
+  {
+    // printf("draw row %d with len %d\n", row, w);
+    lseek(fbdev, x + (y + row) * disp_size.w, SEEK_SET);
+    // printf("pixels pos %p with len %d\n",pixels + row * w, w);
+    write(fbdev, pixels + row * w, w);
+    // printf("draw row %d with len %d\n", row, w);
   }
-  //---------------------
+  write(fbdev, 0, 0);
+}
 
+void NDL_OpenAudio(int freq, int channels, int samples)
+{
+}
+
+void NDL_CloseAudio()
+{
+}
+
+int NDL_PlayAudio(void *buf, int len)
+{
   return 0;
 }
 
-void NDL_Quit() {
+int NDL_QueryAudio()
+{
+  return 0;
+}
+
+int NDL_Init(uint32_t flags)
+{
+  if (getenv("NWM_APP"))
+  {
+    evtdev = 3;
+  }
+  evtdev = open("/dev/events", 0, 0);
+  fbdev = open("/dev/fb", 0, 0);
+  dispinfo_dev = open("/proc/dispinfo", 0, 0);
+
+  // get_disp_size();
+  FILE *fp = fopen("/proc/dispinfo", "r");
+  fscanf(fp, "WIDTH:%d\nHEIGHT:%d\n", &disp_size.w, &disp_size.h);
+  // printf("disp size is %d,%d\n", disp_size.w, disp_size.h);
+  assert(disp_size.w >= 400 && disp_size.w <= 800);
+  assert(disp_size.h >= 300 && disp_size.h <= 640);
+  fclose(fp);
+  return 0;
+}
+
+void NDL_Quit()
+{
+  close(evtdev);
+  close(dispinfo_dev);
 }
